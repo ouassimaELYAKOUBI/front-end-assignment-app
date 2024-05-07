ce87a9e88f2ef393a1ffb774e8ab9b52eb065a1a
ouassima <elyakoubi.ouassima@gmail.com>
2024-05-07 17:32:25 +0100

    fix overlap

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -4,7 +4,7 @@ const Hero = () => {
   return (
     <div className="h-screen w-full relative ">
       
-      <div className='flex flex-col absolute mt-64 md:mt-96 lg:mt-80 w-4/5 md:w-3/4 md:ps-20  gap-y-6 md:gap-y-12 px-6 md:px-0'>
+      <div className='flex flex-col absolute mt-64  w-4/5 md:w-3/4 md:ps-20  gap-y-6 md:gap-y-12 px-6 md:px-0'>
         <h1 className='font-monument font-extrabold text-3xl md:text-5xl uppercase tracking-widest'>The world&apos;s first platform for Tokenizing AI blockchain projects</h1>
             <p className="gradient-border w-fit gradient-text font-satoshi py-2 font-bold text-lg md:text-[22px]">Hold the Creon Pass NFT and earn passive income from AI Tools</p>
       
